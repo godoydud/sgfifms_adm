f064d8af7480ec3c70e983089837d4b7680a82f9
Eduardo Godoy <eduardo.fuso@novaandradina.org>
2019-05-20 23:05:15 -0300

    corrigindo bugs em listar por veiculo

--- public/js/listarPorPlaca.js
+++ public/js/listarPorPlaca.js
@@ -21,22 +21,19 @@ const criarCards = () => {
     
     let placa = document.getElementsByTagName(`select`)[0].value
 
-    firebase.database().ref('veiculos/').once('value', function (snapshot) {
+    firebase.database().ref('viagens/').once('value', function (snapshot) {
         for (let i = 1; i < snapshot.val().length; i++) {
-            // console.log(snapshot.val());
-            // console.log(placa);
-            // console.log(snapshot.val()[i].placaVeiculo);
-
+            console.log(snapshot.val().placa);
             
-            if (placa == snapshot.val()[i].placaVeiculo) {
-                // alert(`Hello`)
-                document.getElementById('retorno').innerHTML = document.getElementById('retorno').innerHTML + `
+            if (placa == snapshot.val()[i].placa) {
+                console.log(snapshot.val().placa);
+                document.getElementById('return').innerHTML = document.getElementById('return').innerHTML + `
             <br></br>
         <ul class="list-group">
             <li class="list-group-item active">ID: ${i}</li>
             <li class="list-group-item"><strong>Motorista: </strong>${snapshot.val()[i].motorista}</li>
-            <li class="list-group-item"><strong>Veículo: </strong>${snapshot.val()[i].nomeVeiculo}</li>
-            <li class="list-group-item"><strong>Placa: </strong>${snapshot.val()[i].placaVeiculo}</li>
+            <li class="list-group-item"><strong>Veículo: </strong>${snapshot.val()[i].veiculo}</li>
+            <li class="list-group-item"><strong>Placa: </strong>${snapshot.val()[i].placa}</li>
             <li class="list-group-item"><strong>Finalidade: </strong>${snapshot.val()[i].finalidade}</li>
         </ul>
 
