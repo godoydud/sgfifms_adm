1fa4bde830ca1f7a6a719f868ad7c6d292543b93
Eduardo Godoy <eduardo.fuso@novaandradina.org>
2019-05-29 10:30:19 -0300

    Exemplo de Commit

--- public/js/listarVeiculos.js
+++ public/js/listarVeiculos.js
@@ -22,7 +22,6 @@ const listarVeiculos = () => {
             li[2].innerHTML = `<strong>Quilometragem: </strong>${veiculos[i][1].kmVeiculo}`
             li[3].className = 'text-center list-group-item'
             li[3].innerHTML = ' '
-       //     li[2].innerHTML =(document.createTextNode(snapshot.val()[i].kmVeiculo))
             button[0].className = 'btn btn-info'
             button[1].className = 'btn btn-danger'
             button[0].type = 'button'
@@ -61,20 +60,6 @@ const listarVeiculos = () => {
 
 
             document.getElementById('return').appendChild(ul)
-/*
-            .innerHTML = document.getElementById('return').innerHTML + `
-            <br></br>
-        <ul class="list-group">
-            <li class="text-center list-group-item active">${snapshot.val()[i].nomeVeiculo}</li>
-            <li class="list-group-item"><strong>Placa: </strong>${snapshot.val()[i].placaVeiculo}</li>
-            <li class="list-group-item"><strong>Quilometragem: </strong>${snapshot.val()[i].kmVeiculo}</li>
-            <li class="text-center list-group-item">
-            <button  type="button" class="btn btn-info"><i class="far fa-edit"></i></button><label>   </label>
-            <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button></li>
-        </ul>
-       
-    <br></br>
-        ` */
         }
     })
 
@@ -86,10 +71,5 @@ function delete_user(element) {
    
  let ul = element.parentElement;
  
-    
-    
     firebase.database().ref().child('veiculos/' + ul.id).remove();
-    //window.confirm("Tem certeza que deseja excluir o veículo?")
-    //alert('Veículo excluído com sucesso!');
-
 }
