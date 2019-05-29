e0e64a77cfa2a58ecf65d64cf3ff69d876df3487
Eduardo <eduardo.fuso@novaandradina.org>
2019-05-28 22:50:00 -0300

    Consertando cadastro de veiculos

--- public/js/cadastrarVeiculo.js
+++ public/js/cadastrarVeiculo.js
@@ -1,9 +1,53 @@
-// Cadastro de Veículos
-// var d = new Date();
-// var t = d.getTime();
-// var counter = t;
+// // Cadastro de Veículos
+// // var d = new Date();
+// // var t = d.getTime();
+// // var counter = t;
 
-document.getElementById("form").addEventListener("submit", (e) => {
+// document.getElementById("form").addEventListener("submit", (e) => {
+//     var nomeVeiculo = document.getElementById("nomeVeiculo").value;
+//     var placaVeiculo = document.getElementById("placaVeiculo").value;
+//     var kmVeiculo = document.getElementById("kmVeiculo").value;
+//     e.preventDefault();
+//     cadastrarVeiculo(nomeVeiculo, placaVeiculo, kmVeiculo);
+//     form.reset();
+// });
+
+// // async function cadastrarVeiculo(nomeVeiculo, placaVeiculo, kmVeiculo) {
+
+// //     await firebase.database().ref("veiculos/" + 1).set({
+// //         nomeVeiculo: nomeVeiculo,
+// //         placaVeiculo: placaVeiculo,
+// //         kmVeiculo: kmVeiculo
+// //     });
+// // }
+
+
+// async function cadastrarVeiculo(nomeVeiculo, placaVeiculo, kmVeiculo) {
+//     let id
+//     await firebase.database().ref('veiculos/').once('value', function (snapshot) {
+//         id = snapshot.numChildren() 
+//         sessionStorage.id = id
+//     })
+// console.log(id);
+
+//     await firebase.database().ref("veiculos/" + id).set({
+//         id: id,
+//         nomeVeiculo: nomeVeiculo,
+//         placaVeiculo: placaVeiculo,
+//         kmVeiculo: kmVeiculo
+//     });
+
+//     alert('Veiculo cadastrado com sucesso!');
+//     window.location.href = ""
+// }
+
+
+// Cadastro de Veículos
+var d = new Date();
+var t = d.getTime();
+var counter = t;
+
+ document.getElementById("form").addEventListener("submit", (e) =>{
     var nomeVeiculo = document.getElementById("nomeVeiculo").value;
     var placaVeiculo = document.getElementById("placaVeiculo").value;
     var kmVeiculo = document.getElementById("kmVeiculo").value;
@@ -12,31 +56,16 @@ document.getElementById("form").addEventListener("submit", (e) => {
     form.reset();
 });
 
-// async function cadastrarVeiculo(nomeVeiculo, placaVeiculo, kmVeiculo) {
+ async function cadastrarVeiculo(nomeVeiculo, placaVeiculo, kmVeiculo){
 
-//     await firebase.database().ref("veiculos/" + 1).set({
-//         nomeVeiculo: nomeVeiculo,
-//         placaVeiculo: placaVeiculo,
-//         kmVeiculo: kmVeiculo
-//     });
-// }
-
-
-async function cadastrarVeiculo(nomeVeiculo, placaVeiculo, kmVeiculo) {
-    let id
-    await firebase.database().ref('veiculos/').once('value', function (snapshot) {
-        id = snapshot.numChildren() 
-        sessionStorage.id = id
-    })
-console.log(id);
-
-    await firebase.database().ref("veiculos/" + id).set({
-        id: id,
+     await firebase.database().ref("veiculos/" + counter).set({
+        id: counter,
         nomeVeiculo: nomeVeiculo,
         placaVeiculo: placaVeiculo,
         kmVeiculo: kmVeiculo
     });
 
-    alert('Veiculo cadastrado com sucesso!');
-    window.location.href = ""
+     alert('Veículo cadastrado com sucesso!');
+    localStorage.id = counter;
+    window.location.href = "cadastrarUser.html"
 }
